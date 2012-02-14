197bf36b5eb0c008e008e9eaff2ddd88d71921f2
Florent <flochtililoch@gmail.com>
2012-02-14 11:46:15 +0100

    moved coverflow modification to core file, cleaned up html / css

---
+++ README.md
@@ -0,0 +1,34 @@
+Coverflow Reworked
+====================
+
+Introduction
+---------------------
+
+Rewrite of Addy Osmani & Paul Bakaus Coverflow to ensure a clean separation of dependencies
+
+
+Requirements (included as submodules)
+---------------------
+
+* jQuery 1.7.1
+* jQuery UI Core 1.8.17
+* jQuery UI Widget 1.8.17
+* HTML5 Boilerplate
+* ui.coverflow.js (from https://github.com/addyosmani/jqueryui-coverflow-ii.git)
+
+Optional (included as submodules)
+---------------------
+
+Slider support :
+
+* jQuery UI Mouse 1.8.17
+* jQuery UI Slider 1.8.17
+
+Cross-browser support (other than webkit) :
+
+*  sylvester.js
+*  transformie.js
+
+Mousewheel support :
+
+*  jquery.mousewheel.js
--- web/js/ui.coverflow.js
+++ web/js/ui.coverflow.js
@@ -4,7 +4,7 @@
  * Maintenance updates for 1.8.9/jQuery core 1.5, 1.6.2 made.
  * Original Component: Paul Bakaus for jQueryUI 1.7 
  */
- (function($) {
+(function($) {
 
   function getPrefix(prop) {
     var prefixes = ['Moz', 'Webkit', 'Khtml', 'O', 'ms'],
@@ -35,8 +35,10 @@
       orientation: 'horizontal',
       item: 0,
       trigger: 'click',
-      center: true,   // If false, element's base position isn't touched in any way
-      recenter: true  // If false, the parent element's position doesn't get animated while items change
+      center: true,
+      // If false, element's base position isn't touched in any way
+      recenter: true
+      // If false, the parent element's position doesn't get animated while items change
     },
 
     _create: function() {
@@ -48,8 +50,8 @@
       this.itemWidth = this.items.width();
       this.itemHeight = this.items.height();
       this.duration = o.duration;
-      this.current = o.item;  // initial item
-
+      this.current = o.item;
+      // initial item
       // Bind click events on individual items
       this.items.bind(o.trigger,
       function() {
@@ -58,11 +60,11 @@
 
       // Center the actual parent's left side within it's parent
       this.element.css(this.props[2],
-        (o.recenter ? -this.current * this.itemSize / 2: 0)
-        + (o.center ? this.element.parent()[0]['offset' + this.props[1]] / 2 - this.itemSize / 2: 0)
-        // Center the items container
-        - (o.center ? parseInt(this.element.css('padding' + this.props[3]), 10) || 0: 0)
-        //Subtract the padding of the items container
+      (o.recenter ? -this.current * this.itemSize / 2: 0)
+      + (o.center ? this.element.parent()[0]['offset' + this.props[1]] / 2 - this.itemSize / 2: 0)
+      // Center the items container
+      - (o.center ? parseInt(this.element.css('padding' + this.props[3]), 10) || 0: 0)
+      //Subtract the padding of the items container
       );
 
       // Jump to the first item
@@ -91,11 +93,11 @@
       };
 
       animation[this.props[2]] = (
-        (this.options.recenter ? -this.current * this.itemSize / 2: 0)
-        + (this.options.center ? this.element.parent()[0]['offset' + this.props[1]] / 2 - this.itemSize / 2: 0)
-        // Center the items container
-        - (this.options.center ? parseInt(this.element.css('padding' + this.props[3]), 10) || 0: 0)
-        // Subtract the padding of the items container
+      (this.options.recenter ? -this.current * this.itemSize / 2: 0)
+      + (this.options.center ? this.element.parent()[0]['offset' + this.props[1]] / 2 - this.itemSize / 2: 0)
+      // Center the items container
+      - (this.options.center ? parseInt(this.element.css('padding' + this.props[3]), 10) || 0: 0)
+      // Subtract the padding of the items container
       );
 
       // Trigger the 'select' event/callback
@@ -142,13 +144,148 @@
       });
       this.element.parent().scrollTop(0);
     },
-    
+
     _uiHash: function() {
       return {
         item: this.items[this.current],
         value: this.current
       };
     }
-    
+
   });
 })(jQuery);
+
+/*!
+ * Inspiration from Addy Osmani - http://addyosmani.com/resources/coverflow/demo/demo.html
+ * Cleanup / ajustments by Florent Bonomo flochtililoch@gmail.com
+ * Addy Osmani, Paul Bakhaus, Nicolas Bonnicci
+ * Dependencies :
+ *  jquery (tested with 1.7.1)
+ *  jquery.ui.core.js (tested with 1.8.17)
+ *  jquery.ui.widget.js (tested with 1.8.17)
+ *  
+ *  jquery.ui.mouse.js  (optional, required if slider support needed)
+ *  jquery.ui.slider.js (optional, required if slider support needed)
+ *
+ *  ui.coverflow.js
+ *
+ *  sylvester.js
+ *  transformie.js
+ *
+ *  jquery.mousewheel.js
+ */
+$.extend(true, $.ui.coverflow.prototype, {
+
+  options: {
+    keyboard: true,
+    mousewheel: true,
+    slider: false,
+    item: 0,
+    duration: 1500,
+    offset: 0,
+    sensitivity: 0.05
+  },
+
+  _init: function() {
+    // Enabled only if Slider library present
+    if (this.options.slider && $.fn.slider !== undefined) {
+      this.initSlider(this.options.slider);
+    }
+
+    this.skipTo(this.options.offset);
+
+    // Enabled only if Mousewheel library present
+    if (this.options.mousewheel && $.fn.mousewheel !== undefined) {
+      this.initMouseWheel();
+    }
+
+    if (this.options.keyboard) {
+      this.initKeyboard();
+    }
+
+    return $.ui.coverflow;
+  },
+
+  initSlider: function(elt) {
+    var self = this;
+    elt.slider({
+      min: 0,
+      max: self.element.find(self.options.items).length - 1,
+      slide: function(event, ui) {
+        self.element.coverflow('select', ui.value, true);
+      }
+    });
+  },
+
+  initMouseWheel: function() {
+    var self = this;
+    $('body').mousewheel(function(event, delta) {
+      var position = self.current,
+      sensitivity = self.options.sensitivity,
+      item = 0,
+      length = self.element.find(self.options.items).length - 1,
+      leftValue = 0;
+
+      // Check the deltas to find out if the user has scrolled up or down
+      if (delta > sensitivity && position > 0) {
+        position -= 1;
+      } else {
+        if (delta < -(sensitivity) && position < length) {
+          position += 1;
+        }
+      }
+      leftValue = -((100 - position) * self.difference / 100);
+
+      // Calculate the content top from the slider position
+      if (leftValue > 0) {
+        leftValue = 0;
+      }
+
+      // Stop the content scrolling down too much
+      if (Math.abs(leftValue) > self.difference) {
+        leftValue = ( - 1) * self.difference;
+      }
+
+      // Stop the content scrolling up beyond point desired
+      item = Math.floor(position);
+      self.skipTo(item);
+
+      // Prevent history back and forth if there are still items
+      if (item >= 0 && item <= length)
+      {
+        // Edge cases
+        if (! (item === length && delta < 0 ||  item === 0 && delta > 0))
+        {
+          event.preventDefault();
+        }
+      }
+    });
+  },
+
+  initKeyboard: function() {
+    var self = this;
+    $(document).keydown(function(e) {
+      var current = self.current;
+      if (e.keyCode == 37) {
+        if (current > 0) {
+          current--;
+          self.skipTo(current);
+        }
+      } else {
+        if (e.keyCode == 39) {
+          if (current < self.element.find(self.options.items).length - 1) {
+            current++;
+            self.skipTo(current);
+          }
+        }
+      }
+    });
+  },
+
+  skipTo: function(itemNumber) {
+    if (this.options.slider && $.fn.slider !== undefined) {
+      this.options.slider.slider("option", "value", itemNumber);
+    }
+    this.element.coverflow('select', itemNumber, true);
+  }
+});
